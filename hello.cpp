a949e011f20e9f9014a95b278e6d541ce0256656
Ronakbedi12 <bedironak822@gmail.com>
2026-02-03 00:37:31 +0530

    Initial commit

--- hello.cpp
+++ hello.cpp
@@ -3,21 +3,24 @@ using namespace std;
 
 int main()
 {
-
     int n;
-    cout << "Enter the value of n" << endl;
+    cout << "Enter value of n: " << endl;
     cin >> n;
-
-    int a = 0;
-    int b = 1;
-
-    cout << a << " " << b << " ";
-
-    for (int i = 1; i <= n; i++)
+    bool is_Prime = 1;
+    for (int i = 2; i < n; i++)
     {
-        int next_number = a + b;
-        cout << next_number << " ";
-        a = b;
-        b = next_number;
+        if (n % i == 0)
+        {
+            is_Prime = 0;
+            break;
+        }
+    }
+    if (is_Prime == 0)
+    {
+        cout << "Not prime. " << endl;
+    }
+    else
+    {
+        cout << "Prime. " << endl;
     }
 }
